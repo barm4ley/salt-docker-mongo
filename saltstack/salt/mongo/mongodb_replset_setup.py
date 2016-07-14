e6fe1f0bbdc2057d948c0eef9b8fadf405ab4c53
barm4ley <mamu@Totenkopf.local>
2016-07-14 16:45:11 +0300

    Added placeholder script to create mongo replset

---
+++ saltstack/salt/mongo/mongodb_replset_setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import __future__
+import pymongo
+import socket
+
+
+print('My name is ', socket.gethostname())
+
+#SERVERS = ['mongo1', 'mongo2']
+#SERVERS_IP = [socket.gethostbyname(svr) for svr in SERVERS]
+
+#client = pymongo.MongoClient()
+
+
