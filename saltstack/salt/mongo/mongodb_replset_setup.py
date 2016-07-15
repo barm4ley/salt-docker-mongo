ae9e288bb606bd15024d28c42e8739f246d2535b
barm4ley <mamu@Totenkopf.local>
2016-07-15 16:45:10 +0300

    Added replica set initialization functionality

--- saltstack/salt/mongo/mongodb_replset_setup.py
+++ saltstack/salt/mongo/mongodb_replset_setup.py
@@ -7,9 +7,29 @@ import socket
 
 print('My name is ', socket.gethostname())
 
+rs_name = 'replset'
+
 #SERVERS = ['mongo1', 'mongo2']
 #SERVERS_IP = [socket.gethostbyname(svr) for svr in SERVERS]
 
-#client = pymongo.MongoClient()
+client = pymongo.MongoClient()
+this_host = socket.gethostname()
 
-
+def init_replset(client, rs_name):
+    conf = client.local.system.replset.find.count()
+    if conf == 0:
+        # replica doesn't exitst
+        cfg = {
+            '_id': rs_name,
+            'members': [
+                {'host': this_host,
+                 '_id': 0,
+                 'arbiterOnly': False}
+            ]
+        }
+        client.admin.command("replSetInitiate", cfg)
+        print("ReplSet initiated:")
+        print(client.local.system.replset.find_one())
+    else:
+        print('ReplSet is already present:')
+        print(client.local.system.replset.find_one())
